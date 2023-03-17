90d48a6ed3eff20e94f13406f2fd24e8b9d47ac8
Laurin van den Bergh <laurin.vandenbergh@uzh.ch>
2023-03-17 17:48:42 +0100

    add loss for shpereface, cosface, and arcface

--- openset_imagenet/losses.py
+++ openset_imagenet/losses.py
@@ -1,7 +1,9 @@
 """ Code taken from the vast library https://github.com/Vastlab/vast"""
-from torch.nn import functional as f
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from vast import tools
+import math
 
 
 class EntropicOpensetLoss:
@@ -27,6 +29,7 @@ class EntropicOpensetLoss:
             )
         )
         return self.cross_entropy(logits, categorical_targets)
+
 
 
 class AverageMeter(object):
@@ -93,20 +96,102 @@ class EarlyStopping:
             self.best_score = score
             self.counter = 0
 
-# TODO
-class ASoftmaxLoss:
-    """A-Softmax loss from ShpereFace paper."""
-    def __init__(self, num_of_classes, unk_weight=1):
-        raise NotImplementedError
 
-    def __call__(self, logits, target):
-        raise NotImplementedError
+# from: https://github.com/ydwen/opensphere/blob/032c31b8918fb7639d3b34ac7433bfd537c6c518/model/head/arcface.py
+class ArcFace(nn.Module):
+    """ reference: <Additive Angular Margin Loss for Deep Face Recognition>
+    """
+    def __init__(self, feat_dim, num_class, s=64., m=0.5):
+        super(ArcFace, self).__init__()
+        self.feat_dim = feat_dim
+        self.num_class = num_class
+        self.s = s
+        self.m = m
+        self.w = nn.Parameter(torch.Tensor(feat_dim, num_class))
+        nn.init.xavier_normal_(self.w)
 
-# TODO
-class LargeMarginCosineLoss:
-    """Large Margin Cosine Loss (LMCL) from CosFace paper."""
-    def __init__(self, num_of_classes, unk_weight=1):
-        raise NotImplementedError
+    def forward(self, x, y):
+        with torch.no_grad():
+            self.w.data = F.normalize(self.w.data, dim=0)
 
-    def __call__(self, logits, target):
-        raise NotImplementedError
+        cos_theta = F.normalize(x, dim=1).mm(self.w)
+        with torch.no_grad():
+            theta_m = torch.acos(cos_theta.clamp(-1+1e-5, 1-1e-5))
+            theta_m.scatter_(1, y.view(-1, 1), self.m, reduce='add')
+            theta_m.clamp_(1e-5, 3.14159)
+            d_theta = torch.cos(theta_m) - cos_theta
+
+        logits = self.s * (cos_theta + d_theta)
+        loss = F.cross_entropy(logits, y)
+
+        return loss
+
+
+# from: https://github.com/ydwen/opensphere/blob/032c31b8918fb7639d3b34ac7433bfd537c6c518/model/head/cosface.py
+class CosFace(nn.Module):
+    """reference1: <CosFace: Large Margin Cosine Loss for Deep Face Recognition>
+       reference2: <Additive Margin Softmax for Face Verification>
+    """
+    def __init__(self, feat_dim, num_class, s=64., m=0.35):
+        super(CosFace, self).__init__()
+        self.feat_dim = feat_dim
+        self.num_class = num_class
+        self.s = s
+        self.m = m
+        self.w = nn.Parameter(torch.Tensor(feat_dim, num_class))
+        nn.init.xavier_normal_(self.w)
+
+    def forward(self, x, y):
+        with torch.no_grad():
+            self.w.data = F.normalize(self.w.data, dim=0)
+
+        cos_theta = F.normalize(x, dim=1).mm(self.w)
+        with torch.no_grad():
+            d_theta = torch.zeros_like(cos_theta)
+            d_theta.scatter_(1, y.view(-1, 1), -self.m, reduce='add')
+
+        logits = self.s * (cos_theta + d_theta)
+        loss = F.cross_entropy(logits, y)
+
+        return loss
+
+
+# from: https://github.com/ydwen/opensphere/blob/032c31b8918fb7639d3b34ac7433bfd537c6c518/model/head/sphereface.py
+class SphereFace(nn.Module):
+    """ reference: <SphereFace: Deep Hypersphere Embedding for Face Recognition>"
+        It also used characteristic gradient detachment tricks proposed in
+        <SphereFace Revived: Unifying Hyperspherical Face Recognition>.
+    """
+    def __init__(self, feat_dim, num_class, s=30., m=1.5):
+        super(SphereFace, self).__init__()
+        self.feat_dim = feat_dim
+        self.num_class = num_class
+        self.s = s
+        self.m = m
+        self.w = nn.Parameter(torch.Tensor(feat_dim, num_class))
+        nn.init.xavier_normal_(self.w)
+
+    def forward(self, x, y):
+        # weight normalization
+        with torch.no_grad():
+            self.w.data = F.normalize(self.w.data, dim=0)
+
+        # cos_theta and d_theta
+        cos_theta = F.normalize(x, dim=1).mm(self.w)
+        with torch.no_grad():
+            m_theta = torch.acos(cos_theta.clamp(-1.+1e-5, 1.-1e-5))
+            m_theta.scatter_(
+                1, y.view(-1, 1), self.m, reduce='multiply',
+            )
+            k = (m_theta / math.pi).floor()
+            sign = -2 * torch.remainder(k, 2) + 1  # (-1)**k
+            phi_theta = sign * torch.cos(m_theta) - 2. * k
+            d_theta = phi_theta - cos_theta
+
+        logits = self.s * (cos_theta + d_theta)
+        loss = F.cross_entropy(logits, y)
+
+        return loss
+
+
+
